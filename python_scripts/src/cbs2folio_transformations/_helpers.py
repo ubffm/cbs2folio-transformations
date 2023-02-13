a53a550e392d555f42762994c6bf853b123c8053
JensHeinrich <github.com/JensHeinrich>
2023-02-13 15:04:53 +0100

    Suppress warning for TypeVar
    TypeVar is used in docstring

--- python_scripts/src/cbs2folio_transformations/_helpers.py
+++ python_scripts/src/cbs2folio_transformations/_helpers.py
@@ -6,7 +6,9 @@ _ExceptionType = TypeVar("_ExceptionType", bound=Exception)
 
 
 # Source: https://stackoverflow.com/questions/9157210/how-do-i-raise-the-same-exception-with-a-custom-message-in-python  # noqa: E501
-def reraise(e: _ExceptionType, info: str) -> NoReturn:
+def reraise(
+    e: _ExceptionType, info: str
+) -> NoReturn:  # pyright: reportInvalidTypeVarUse=false
     """Reraise an exception after adding information.
 
     Args:
