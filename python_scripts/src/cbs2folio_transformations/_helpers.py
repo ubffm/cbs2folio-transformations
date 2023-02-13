5f179d6aa111a96ee3fb143948d04ab104f47314
JensHeinrich <github.com/JensHeinrich>
2023-02-13 10:26:06 +0100

    Create module for helpers
    Add reraise helper

---
+++ python_scripts/src/cbs2folio_transformations/_helpers.py
@@ -0,0 +1,21 @@
+"""Module for reusable codesnippets."""
+from typing import NoReturn, TypeVar
+
+_ExceptionType = TypeVar("_ExceptionType", bound=Exception)
+
+# Source: https://stackoverflow.com/questions/9157210/how-do-i-raise-the-same-exception-with-a-custom-message-in-python
+def reraise(e: _ExceptionType, info: str) -> NoReturn:
+    """Reraise an exception after adding information.
+
+    Args:
+        e (_ExceptionType): Exception to reraise
+        info (str): Additional information
+
+    Raises:
+        e.with_traceback (_ExceptionType): The appended Exception
+
+    Returns:
+        NoReturn: Does not return
+    """
+    e.args = info, *e.args
+    raise e.with_traceback(e.__traceback__)
