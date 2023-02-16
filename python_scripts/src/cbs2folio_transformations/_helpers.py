4df4da440ebfd8a9750fc737bf49ff56db41c623
JensHeinrich <github.com/JensHeinrich>
2023-02-16 12:26:40 +0100

    Add helper for xsl params

--- python_scripts/src/cbs2folio_transformations/_helpers.py
+++ python_scripts/src/cbs2folio_transformations/_helpers.py
@@ -63,3 +63,46 @@ def get_variable_from_xsl(variable_name: str, xsl: etree._ElementTree) -> str:
                 return _value.strip("'")
 
     raise ValueError(f"{variable_name} not defined in XSL")
+
+
+# Based on: https://www.mail-archive.com/lxml@python.org/msg00011.html
+def get_param_default_from_xsl(
+    param_name: str, xsl: etree._ElementTree
+) -> str:
+    """Get the value of an XSL variable.
+
+    Args:
+        variable_name (str): Name of the variable to find
+        xsl (etree._ElementTree): XSL Tree to search in
+
+    Raises:
+        ValueError: Unkmown variable
+
+    Returns:
+        str: Value of the variable
+    """
+    xpath_results = xsl.xpath(
+        "//xsl:param[@name=$name]",
+        name=param_name,
+        namespaces={"xsl": "http://www.w3.org/1999/XSL/Transform"},
+    )
+
+    for xpath_result in xpath_results:
+        if xpath_result.get("name") == param_name:
+            for xpath_child in xpath_result.xpath(
+                "xsl:value-of",
+                namespaces={"xsl": "http://www.w3.org/1999/XSL/Transform"},
+            ):
+                if "select" in xpath_child.keys():
+                    return xpath_child.get("select").strip("'")
+                if "text" in xpath_child.keys():
+                    return xpath_child.text.strip("'")
+
+            if "select" in xpath_result.attrib:
+                return xpath_result.attrib["select"].strip("'")
+
+            if hasattr(xpath_result, "text") and (_value := xpath_result.text):
+                # _value: str = xpath_result.text
+                return _value.strip("'")
+
+    raise ValueError(f"{param_name} not defined in XSL")
--- python_scripts/tests/test_helpers.py
+++ python_scripts/tests/test_helpers.py
@@ -4,6 +4,7 @@ from typing import List
 from typing import Tuple
 
 import pytest
+from cbs2folio_transformations._helpers import get_param_default_from_xsl
 from cbs2folio_transformations._helpers import get_variable_from_xsl
 from lxml import etree  # nosec blacklist
 
@@ -81,4 +82,56 @@ NAMESPACES = {"xsl": "http://www.w3.org/1999/XSL/Transform"}
 def test_get_variable_from_xsl(variable_name, xsl, value):
     assert (  # nosec assert_used
         get_variable_from_xsl(variable_name=variable_name, xsl=xsl) == value
+    )
+
+
+@pytest.mark.parametrize(
+    "param_name,xsl,value",
+    [
+        pytest.param(
+            "token-markers",
+            etree.fromstring(
+                """<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:exsl="http://exslt.org/common"
+  version="1.1"
+  exclude-result-prefixes="exsl"
+  >
+  <!-- Normalize a signature or range -->
+  <xsl:template name="normalize-signature-or-range">
+    <xsl:param name="text"/>
+    <xsl:param name="token-markers" select="' /.:'" />
+    <xsl:variable name="token-replacement">
+      <xsl:call-template name="replace-with-space">
+        <xsl:with-param name="text" select="$token-markers"/>
+      </xsl:call-template>
+    </xsl:variable>
+    <xsl:if test="$debug-template-logic-verbosity">
+      <xsl:message>Debug:
+        Text: "<xsl:value-of select="$text"/>"
+        Normalized: "<xsl:value-of
+        select="normalize-space(
+            translate($text, $token-markers, $token-replacement )
+            )"
+        />"
+        Markers: "<xsl:value-of select="$token-markers"/>"
+        Replacement: "<xsl:value-of select="$token-replacement"/>"
+      </xsl:message>
+    </xsl:if>
+      <xsl:value-of
+      select="normalize-space(
+        translate($text, $token-markers, $token-replacement )
+        )"
+        />
+  </xsl:template>
+</xsl:stylesheet>
+                    """.encode()
+            ),
+            " /.:",
+        )
+    ],
+)
+def test_get_param_default_from_xsl(param_name, xsl, value):
+    assert (  # nosec assert_used
+        get_param_default_from_xsl(param_name=param_name, xsl=xsl) == value
     )
