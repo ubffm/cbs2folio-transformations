e1a31a879e4bb21e48fb0a3a2a4d363242c912f1
JensHeinrich <github.com/JensHeinrich>
2023-08-15 17:10:06 +0200

    Tweak test helpers
    Create dedicated representation of differences for range nodes.
    Tweak typing.
    Clean up.

--- python_scripts/tests/conftest.py
+++ python_scripts/tests/conftest.py
@@ -3,8 +3,10 @@ import io
 import logging
 import pathlib
 from collections.abc import Iterable
+from typing import Dict
+from typing import List
 from typing import Optional
-from typing import TypeVar
+from typing import Set
 
 import pytest
 from cbs2folio_transformations._test_helpers import apply_transformations
@@ -20,7 +22,7 @@ from cbs2folio_transformations._test_helpers import (
 from cbs2folio_transformations.csv2holdingsxslt import (
     create_holdings_items_xsl_from_csv,
 )
-from defusedxml import ElementTree
+from defusedxml import ElementTree  # type: ignore[import]
 from lxml import etree  # nosec blacklist
 
 logger = logging.getLogger()
@@ -77,24 +79,21 @@ def pytest_generate_tests(metafunc: pytest.Metafunc):
     else:
         raise ValueError("No XSL specified.")
 
+    _data: Iterable[SignatureExample]
     if (
         hasattr(metafunc.cls, "data_csv_path")
         and metafunc.cls.data_csv_path is not None
     ):
-        _data: Iterable[SignatureExample] = inject_test_data(
-            metafunc.cls.data_csv_path
-        )
+        _data = inject_test_data(metafunc.cls.data_csv_path)
     elif hasattr(metafunc.cls, "data") and metafunc.cls.data is not None:
-        _data: Iterable[
-            SignatureExample
-        ] = yield_signature_example_from_data_iterable(metafunc.cls.data)
+        _data = yield_signature_example_from_data_iterable(metafunc.cls.data)
     else:
         raise ValueError("No data specified.")
 
     for d in _data:
         _dict = {
             k: v
-            for k, v in d.dict(by_alias=True).items()
+            for k, v in d.model_dump(by_alias=True).items()
             if k not in ["id", "marks"]
         }
         argnames = list(_dict.keys()) + ["xsl"]
@@ -110,7 +109,7 @@ def pytest_generate_tests(metafunc: pytest.Metafunc):
 
 
 @pytest.fixture(scope="session")
-def initial_record() -> etree.Element:
+def initial_record() -> etree._Element:
     """Create an record Element from the EXAMPLE_XML.
 
     Returns:
@@ -119,9 +118,13 @@ def initial_record() -> etree.Element:
     parser = etree.XMLParser(remove_blank_text=True)
 
     with open(EXAMPLE_XML) as f:
-        _tree: ElementTree = ElementTree.parse(f, parser=parser)
+        _tree: etree._ElementTree = ElementTree.parse(f, parser=parser)
 
     _record = _tree.find(".//record")
+    if _record is None:
+        raise ValueError(
+            f"Could not find 'record' in {etree.tostring(_tree).decode()}"
+        )
     return _record
 
 
@@ -160,7 +163,7 @@ def record(
     signature: str,
     indicator: str,
     location: Optional[str] = "LOCATION",
-) -> etree.Element:
+) -> etree._Element:
     """Create a record from nothing.
 
     Args:
@@ -179,9 +182,6 @@ def record(
         indicator,
         location,
     )
-
-
-TreeOrElement = TypeVar("TreeOrElement", etree.Element, etree.ElementTree)
 
 
 @pytest.fixture(scope="session")
@@ -289,7 +289,7 @@ def xslt_step6(iln: int) -> etree.XSLT:
 
 
 @pytest.fixture()
-def xslt(xsl: etree.Element) -> etree.XSLT:
+def xslt(xsl: "etree._ElementOrTree") -> etree.XSLT:
     """Generate an transformation from an XSL.
 
     Args:
@@ -324,7 +324,7 @@ def create_example_and_apply_for_step_4(
     xslt_step2: etree.XSLT,
     xslt_step3: etree.XSLT,
     hrid: Optional[int] = None,
-) -> etree.Element:
+) -> etree._ElementTree:
     """Create an example using the parameters and apply the transformation.
 
     Args:
@@ -338,7 +338,7 @@ def create_example_and_apply_for_step_4(
         xslt_step1 (etree.XSLT): transformation for 1st step
         xslt_step2 (etree.XSLT): transformation for 2nd step
         xslt_step3 (etree.XSLT): transformation for 3rd step
-        hrid (Optional[int]): HEBIS wide identifier. Defaults to None.
+        hrid (Optional[int]): HeBIS wide identifier. Defaults to None.
 
     Raises:
         etree.XSLTApplyError: Error applying the transformation
@@ -350,3 +350,228 @@ def create_example_and_apply_for_step_4(
     return apply_transformations(
         _input, [xslt_step1, xslt_step2, xslt_step3, xslt]
     )
+
+
+def _compare_eq_etree_Element_department(
+    left: etree._Element, right: etree._Element, verbose: int = 0
+) -> List[str]:
+    explanation: List[str] = []
+    assert left.attrib["code"] == right.attrib["code"]  # nosec: ignore[B101]
+
+    explanation += [f"Difference in department {str(left.attrib['code'])}:"]
+
+    _left_prefixes: Dict[str, etree._Element] = {
+        node.text: node for node in left if node.tag == "prefix" and node.text
+    }
+    _right_prefixes: Dict[str, etree._Element] = {
+        node.text: node for node in right if node.tag == "prefix" and node.text
+    }
+
+    if _extra_prefixes_left := _left_prefixes.keys() - _right_prefixes.keys():
+        explanation += [f"Extra prefixes in {left}"]
+        explanation += [
+            str(_left_prefixes[prefix]) for prefix in _extra_prefixes_left
+        ]
+
+    if _extra_prefixes_right := _right_prefixes.keys() - _left_prefixes.keys():
+        explanation += [f"Extra prefixes in {right}"]
+        explanation += [
+            str(_right_prefixes[prefix]) for prefix in _extra_prefixes_right
+        ]
+
+    _common_prefixes: Set[str] = set(_left_prefixes.keys()).intersection(
+        _right_prefixes.keys()
+    )
+
+    if _common_prefixes:
+        explanation += ["Differences in prefixes:"]
+        explanation += [
+            f"Different location for {prefix}: {_location_left!r} != {_location_right!r}"  # noqa: E501
+            for prefix in _common_prefixes
+            if (_location_left := _left_prefixes[prefix].attrib["location"])
+            != (_location_right := _right_prefixes[prefix].attrib["location"])
+        ]
+
+    def _create_range_repr(node: etree._Element) -> str:
+        _from = node.attrib["from"]
+        _to = node.attrib["to"]
+        return f"{_from!s}---{_to!s}"
+
+    _left_ranges = {
+        _create_range_repr(node): node for node in left if node.tag == "range"
+    }
+    _right_ranges = {
+        _create_range_repr(node): node for node in right if node.tag == "range"
+    }
+
+    def _repr_range(range: etree._Element) -> str:
+        return f"{range.tag}: {range.attrib}"
+
+    if _extra_ranges_left := _left_ranges.keys() - _right_ranges.keys():
+        explanation += [f"Extra ranges in {left}"]
+        explanation += [
+            _repr_range(_left_ranges[range]) for range in _extra_ranges_left
+        ]
+
+    if _extra_ranges_right := _right_ranges.keys() - _left_ranges.keys():
+        explanation += [f"Extra ranges in {right}"]
+        explanation += [
+            _repr_range(_right_ranges[range]) for range in _extra_ranges_right
+        ]
+
+    _common_ranges: Set[str] = set(_left_ranges.keys()).intersection(
+        _right_ranges.keys()
+    )
+
+    if _common_ranges:
+        explanation += ["Differences in ranges:"]
+        explanation += [
+            f"Different location for {range}: {_location_left!r} != {_location_right!r}"  # noqa: E501
+            for range in _common_ranges
+            if (_location_left := _left_ranges[range].attrib["location"])
+            != (_location_right := _right_ranges[range].attrib["location"])
+        ]
+    return explanation
+
+
+def _compare_eq_etree_Element_ranges(
+    left: etree._Element, right: etree._Element, verbose: int = 0
+) -> List[str]:
+    explanation: List[str] = []
+
+    explanation += [
+        f"  {left} != {right}",
+    ]
+
+    if left.tag != right.tag:
+        explanation += [f"Differing tags: {left.tag} != {right.tag}"]
+
+    if left.attrib != right.attrib:
+        explanation += [
+            f"Differing attributes: {left.attrib} != {right.attrib}"
+        ]
+
+    _left_departments: Dict[str, etree._Element] = {
+        str(node.attrib["code"]): node
+        for node in left
+        if node.tag == "department"
+    }
+    _right_departments: Dict[str, etree._Element] = {
+        str(node.attrib["code"]): node
+        for node in right
+        if node.tag == "department"
+    }
+
+    _extra_departments_left = (
+        _left_departments.keys() - _right_departments.keys()
+    )
+    _extra_departments_right = (
+        _right_departments.keys() - _left_departments.keys()
+    )
+
+    def _repr_department(department: etree._Element) -> str:
+        return f"{department.tag}: {department.attrib}"
+
+    if _extra_departments_left:
+        explanation += [f"Extra departments in {left}:"]
+        explanation += [
+            _repr_department(_left_departments[code])
+            for code in _extra_departments_left
+        ]
+
+    if _extra_departments_right:
+        explanation += [f"Extra departments in {right}:"]
+        explanation += [
+            _repr_department(_right_departments[code])
+            for code in _extra_departments_right
+        ]
+
+    _common_departments = set(_left_departments.keys()).intersection(
+        set(_right_departments.keys())
+    )
+
+    explanation += ["Differences in departments:"]
+
+    if _common_departments:
+        for code in _common_departments:
+            if _left_departments[code] != _right_departments[code]:
+                explanation += _compare_eq_etree_Element_department(
+                    _left_departments[code],
+                    _right_departments[code],
+                    verbose,
+                )
+
+    _left_children: Set[etree._Element] = {
+        node for node in left if node.tag != "department"
+    }
+    _right_children: Set[etree._Element] = {
+        node for node in right if node.tag != "department"
+    }
+    if _left_children != _right_children:
+        if _extra_left := _left_children - _right_children:
+            explanation += ["Extra children in left:"]
+            explanation += [
+                etree.tostring(child).decode() for child in _extra_left
+            ]
+
+        if _extra_right := _right_children - _left_children:
+            explanation += ["Extra children in right:"]
+            explanation += [
+                etree.tostring(child).decode() for child in _extra_right
+            ]
+
+    return explanation
+
+
+def _compare_eq_etree_Element(
+    left: etree._Element, right: etree._Element, verbose: int = 0
+) -> List[str]:
+    """Comparison explanation for Etree._Element objects.
+
+    Based on code from pytest.
+
+    Args:
+        left (etree._Element): left XML Node.
+        right (etree._Element): right XML Node.
+        verbose (int, optional): level of verbosity. Defaults to 0.
+
+    Returns:
+        List[str]: Explanation of the difference in nodes.
+    """
+    explanation: List[str] = []
+
+    explanation += [
+        f"  {left} != {right}",
+    ]
+
+    if left.tag != right.tag:
+        explanation += [f"Differing tags: {left.tag} != {right.tag}"]
+    elif left.tag == "ranges":
+        explanation += _compare_eq_etree_Element_ranges(left, right, verbose)
+
+    return explanation
+
+
+def pytest_assertrepr_compare(
+    op, left, right, verbose: int = 0
+) -> List[str] | None:  # noqa: ignore[C901]
+    """Create a proper representation for comparison.
+
+    Args:
+        op (str): comparison operator.
+        left (Any): left object.
+        right (_type_): right object.
+        verbose (int, optional): Level of verbosity. Defaults to 0.
+
+    Returns:
+        List[str] | None: Explanation of the differences between left and right.
+    """  # noqa: E501
+    if (
+        op == "=="
+        and isinstance(left, etree._Element)
+        and isinstance(right, etree._Element)
+    ):
+        return _compare_eq_etree_Element(left, right, verbose)
+
+    logger.warning(f"No dedicated representation for {left}, {right}, {op}")
+    return None
